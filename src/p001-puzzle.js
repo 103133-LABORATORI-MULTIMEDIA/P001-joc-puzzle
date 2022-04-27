31088dc479efe5e09351f9e6ca000ae9d64c65dd
rems <srpiroliro21@gmail.com>
2022-04-27 20:10:21 +0200

    done

--- src/p001-puzzle.js
+++ src/p001-puzzle.js
@@ -1,15 +1,19 @@
+// valors per defecte
+var numFiles=2, numColumnes=2, rightPlaceCnt=0;
+var nomImatge="img-2", extImatge=".jpg"; 
+var fsolved=false, solved=false;
 
-var numFiles=2;
-var numColumnes=2;
-var nomImatge = "img-2";
-var extImatge = ".jpg";
-var audio = {"Avengers":new Audio("audio\vengadores.mp3")};
+let audio={
+    "Avengers":new Audio("audio/vengadores.mp3"),
+    "Tada":new Audio("audio/tada.mp3")
+};
+
 
 $.fn.flashUnlimited=function(){
     $(this).fadeTo(500,0.3,function(){
         $(this).fadeTo(500,1, $(this).flashUnlimited); 
     });
-} 
+}
 
 $(document).ready(function(){
     $("#jugar").on("click",function(){
@@ -17,44 +21,43 @@ $(document).ready(function(){
         $("#felicitacio").hide();
         
         $(".peca").css("transition", "none");
-
-        numFiles = $("#files").val();
-        numColumnes = $("#columnes").val();
-
-        console.log(numFiles, numColumnes);
+        
+        numFiles=$("#files").val(); numColumnes=$("#columnes").val();
 
         creaPuzzle();
         $(".peca")
         .mousedown(function(){
-            zIndexPeca = $(this).css("z-index");
+            zIndexPeca=$(this).css("z-index");
             $(this).css("z-index",100);
         })
         .mouseup(function(){
             posicionaPeca($(this));
-            if(puzzleResolt()){
-                $("#felicitacio").show();  
-                $("#felicitacio h1").flashUnlimited();    
-                audio["Avengers"].play();
+            if(puzzleResolt() && !fsolved){
+                felicitacio();
             }
         });
 
     });    
     $("#resolPuzzle").on("click",function(){
-        resolPuzzle();
-    });
-    $("#nouPuzzle").on("click",function(){
-        location.reload(); // optim?
+        fsolved=true; resolPuzzle();
     });
 
+    $("#nouPuzzle").on("click", function(){ location.reload(); });
+
     $("#form-menu img").on("click",function(){
-        console.log(this.attr("src"));
+        $("#"+nomImatge).css("border-color", "transparent");
+        $(this).css("border-color", "black");
+        nomImatge=$(this).attr("id");
     });    
 });
 
+
 function creaPuzzle(){
+    updateCounter();
+
     $("#form-joc").show();
-    ampladaPeca = Math.floor($("#p-"+nomImatge).width()/numColumnes);
-    alcadaPeca = Math.floor($("#p-"+nomImatge).height()/numFiles);
+    ampladaPeca=Math.floor($("#p-"+nomImatge).width()/numColumnes);
+    alcadaPeca=Math.floor($("#p-"+nomImatge).height()/numFiles);
 
     $("#peces-puzzle").html(crearPeces());
     $(".peca").css({
@@ -64,11 +67,18 @@ function creaPuzzle(){
     
     setImatgePosicioPeces();
    
-	$("#marc-puzzle").css("width", (ampladaPeca*numColumnes)+"px");
-	$("#marc-puzzle").css("height",( alcadaPeca*numFiles)+"px");
-    $("#solucio").css("width", "100%");
-    $("#solucio").css("height","100%");
-    $("#solucio").css("background-image","url(img/"+nomImatge+ extImatge+")");
+	$("#marc-puzzle").css({
+        "width":(ampladaPeca*numColumnes)+"px", 
+        "height":(alcadaPeca*numFiles)+"px"
+    });
+
+    $("#solucio").css({
+        "width":"100%",
+        "height":"100%"
+    });
+
+    if (!$("#difficult").is(":checked"))
+        $("#solucio").css("background-image","url(img/"+nomImatge+ extImatge+")");
 
     $(".peca").draggable();
 }
@@ -88,49 +98,53 @@ function setImatgePosicioPeces(){
     $(".peca").css("background-image","url(img/"+nomImatge+ extImatge+")");
     for (let fila=0; fila<numFiles; fila++){
         for (let columna=0; columna<numColumnes; columna++){
-            $("#f"+fila+"c"+columna).css("background-position", (-(columna)*ampladaPeca)+"px "+(-(fila)*alcadaPeca)+"px");   
-            $("#f"+fila+"c"+columna).css("left", Math.floor(Math.random()*((numColumnes-1)*ampladaPeca))+"px ");
-            $("#f"+fila+"c"+columna).css("top", Math.floor(Math.random()*((numFiles-1)*alcadaPeca))+"px ");
-            $("#f"+fila+"c"+columna).css("z-index","10");
+            $("#f"+fila+"c"+columna).css({
+                "background-position":(-(columna)*ampladaPeca)+"px "+(-(fila)*alcadaPeca)+"px",
+                "left":Math.floor(Math.random()*((numColumnes-1)*ampladaPeca))+"px ",
+                "top": Math.floor(Math.random()*((numFiles-1)*alcadaPeca))+"px ",
+                "z-index":"10"
+            });
         }        
     }   
 }
 
 function posicionaPeca(peca){
-   
-    let posicioPeca = peca.position();
+    let posicioPeca=peca.position();
     let elem_id=peca.attr('id');
 
-    let row=parseInt(elem_id[1]);
-    let col=parseInt(elem_id[3]);
-
-    let h=$("#marc-puzzle").height()
-    let l=$("#marc-puzzle").width()
-
     posicioPecaCorrecte={
-        left:col*l/numColumnes, // x
-        top:row*h/numFiles // y
+        left:parseInt(elem_id[3])*$("#marc-puzzle").width()/numColumnes,    // x
+        top:parseInt(elem_id[1])*$("#marc-puzzle").height()/numFiles        // y
     };
-    if (distanciaDosPunts(posicioPeca, posicioPecaCorrecte)<50){  
-        console.log(elem_id, "correcte!");
-        peca.css("left", posicioPecaCorrecte.left+"px ");
-        peca.css("top", posicioPecaCorrecte.top+"px "); 
-        peca.css("z-index","1");
+    
+
+    if (
+        distanciaDosPunts(posicioPeca, posicioPecaCorrecte)<50 && 
+        !$(peca).data("ui-draggable").options.disabled
+    ){ 
+        peca.css({
+            "left":posicioPecaCorrecte.left+"px ",
+            "top":posicioPecaCorrecte.top+"px ",
+            "z-index":"1"
+        });
+
         peca.draggable("disable");
+        audio["Tada"].play();
+        rightPlaceCnt++; updateCounter();
     }
 }
 
-
 function resolPuzzle(){
-    $(".peca").css("transition", "all 2s");
-    let h=$("#marc-puzzle").height(), l=$("#marc-puzzle").width();
+    $(".peca").draggable("disable").css("transition", "all 1s");
     for (let fila=0; fila<numFiles; fila++){
         for (let columna=0; columna<numColumnes; columna++){
-            $("#f"+fila+"c"+columna).css("top", (fila*l/numFiles)+"px");
-            $("#f"+fila+"c"+columna).css("left", (columna*l/numColumnes)+"px");
+            $("#f"+fila+"c"+columna).css({
+                "top":(fila*$("#marc-puzzle").height()/numFiles)+"px",
+                "left":(columna*$("#marc-puzzle").width()/numColumnes)+"px"
+            });
         }        
-   }
-   
+    }
+    deshabilitaBoto("resolPuzzle");
 }
 
 function puzzleResolt(){
@@ -146,9 +160,26 @@ function puzzleResolt(){
             }
         }        
    }
+   deshabilitaBoto("resolPuzzle");
    return true
 }
 
 function distanciaDosPunts(puntA, puntB){
     return (Math.sqrt( Math.pow( puntB.left-puntA.left,2 ) + Math.pow( puntB.top-puntA.top ,2) ));
+}
+
+function deshabilitaBoto(id){
+    $("#"+id).prop("disabled", true).css("cursor", "not-allowed");
+}
+
+function updateCounter(){
+    $("#counter").text(rightPlaceCnt+"/"+(numFiles*numColumnes));
+}
+
+function felicitacio(){
+    $("#felicitacio").show();  
+    $("#felicitacio h1").flashUnlimited();    
+    audio["Avengers"].play();
+
+    $("#counter").css("color", "green");
 }
